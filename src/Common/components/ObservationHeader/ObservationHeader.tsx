5652d50fdcdec08414d686a1634804e3153bab83
dineshdevagalla <dinesh.nanda00@gmail.com>
2020-07-20 22:50:35 +0530

    first commit observation header in progress

---
+++ src/Common/components/ObservationHeader/ObservationHeader.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { withTranslation, WithTranslation } from 'react-i18next'
+interface RoleType {
+   roleType:"string"
+
+}
+
+
+class ObservationHeader extends Component {
+   renderingObservationHeader=()=>{
+
+
+   
+   }
+   render() {
+      return(
+
+
+
+
+      )
+   }
+}
+
+export default ObservationHeader
